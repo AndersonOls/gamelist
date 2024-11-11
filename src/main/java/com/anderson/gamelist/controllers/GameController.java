01be4883cbb66e052497b29a53a337216e52a0cb
Anderson <anderson.silva1996@hotmail.com>
2024-11-11 19:10:01 -0300

    Implementação do GameRepository, GameService. GameController

--- src/main/java/com/anderson/gamelist/controllers/GameController.java
+++ src/main/java/com/anderson/gamelist/controllers/GameController.java
@@ -1,9 +1,7 @@
 package com.anderson.gamelist.controllers;
 
 import com.anderson.gamelist.dto.GameMinDTO;
-import com.anderson.gamelist.entities.Game;
 import com.anderson.gamelist.services.GameService;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
--- src/main/java/com/anderson/gamelist/services/GameService.java
+++ src/main/java/com/anderson/gamelist/services/GameService.java
@@ -3,7 +3,6 @@ package com.anderson.gamelist.services;
 import com.anderson.gamelist.dto.GameMinDTO;
 import com.anderson.gamelist.entities.Game;
 import com.anderson.gamelist.repositories.GameRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
